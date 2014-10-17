0daaf378ea50729e09d5b1890463efe74f82c40f
Taavi Päll <tapa741@gmail.com>
2014-10-17 15:41:53 +0300

    publishing deck

---
+++ setup.R
@@ -0,0 +1,7 @@
+library(slidify)
+setwd("~/Dropbox/Onkobioloogia/")
+author("Rakutsyklikontroll")
+
+setwd("~/Dropbox/Onkobioloogia/Rakutsyklikontroll/")
+# tee githubis sama nimega tühi repo enne publitseerimist
+publish(repo="Rakutsyklikontroll", user="tpall")
