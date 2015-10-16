41fe176e09e5aecd465b57824b02b3975c42fc17
Taavi Päll <tapa741@gmail.com>
2015-10-16 11:17:46 +0300

    added library

--- setup.R
+++ setup.R
@@ -1,9 +1,11 @@
 library(slidify)
+library(slidifyLibraries)
 # setwd("~/Dropbox/Onkobioloogia/")
 # author("Rakutsyklikontroll")
 
 setwd("~/Dropbox/Onkobioloogia/Rakutsyklikontroll/")
 slidify("index.Rmd")
 browseURL("index.html")
+
 # tee githubis sama nimega tühi repo enne publitseerimist
-publish(repo="Rakutsyklikontroll", user="tpall")
+# publish(repo="Rakutsyklikontroll", user="tpall")
